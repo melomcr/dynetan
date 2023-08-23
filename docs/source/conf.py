e0527ad73f64188c22148493bd086a07f9ea7362
melomcr <1561126+melomcr@users.noreply.github.com>
2023-08-22 23:51:44 -0400

    Updated docs conf for sphynx theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,6 +94,8 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
